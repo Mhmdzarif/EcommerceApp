a448efe0f0676c3deae4437b3599c4dba38181fd
Mhmdzarif <mhmdzrf@gmail.com>
2025-09-12 02:58:26 +0300

    Update AuthResponse.java

--- src/main/java/com/project/EcommerceApp/dto/AuthResponse.java
+++ src/main/java/com/project/EcommerceApp/dto/AuthResponse.java
@@ -1,15 +1,12 @@
 package com.project.EcommerceApp.dto;
 
-public class AuthResponse {
-  public String token;
-  public Long id;
-  public String email;
-  public String role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-  public AuthResponse(String token, Long id, String email, String role) {
-    this.token = token;
-    this.id = id;
-    this.email = email;
-    this.role = role;
-  }
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+  private String token;
+  private String email;
+  private String role;
 }
