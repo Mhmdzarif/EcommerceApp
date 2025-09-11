30de82d3f11556f6c85cfb3e062aa9808c965ae7
Mhmdzarif <mhmdzrf@gmail.com>
2025-09-12 02:55:51 +0300

    Update User.java

--- src/main/java/com/project/EcommerceApp/entity/User.java
+++ src/main/java/com/project/EcommerceApp/entity/User.java
@@ -3,24 +3,22 @@ package com.project.EcommerceApp.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
-@Entity @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(unique = true, nullable = false)
+  @Column(nullable = false, unique = true)
   private String email;
 
   @Column(nullable = false)
   private String password;
 
-  @Enumerated(EnumType.STRING)
   @Column(nullable = false)
-  private Role role = Role.USER;
-
-  public enum Role {
-    USER, ADMIN
-  }
+  private String role;
 }
