70a19a7f4896efb6ed848693591148223a05ce6f
MhmdZarif <mhmdzrf@gmail.com>
2025-09-13 19:35:40 +0300

    Added the Backend part of the project

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
-  static String get apiBase => dotenv.env['API_BASE']!;
+   static const apiBase = 'http://10.0.2.2:5264';
   static int get taxRatePercent => int.tryParse(dotenv.env['TAX_RATE_PERCENT'] ?? '0') ?? 0;
 }
